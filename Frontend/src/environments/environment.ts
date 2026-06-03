f2bf344d0f94c409e4c061d350a45988ec68f92d
Jorge Victor <jorgekvictor26@gmail.com>
2026-06-03 17:59:57 +0100

    Environment aps

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -2,6 +2,5 @@ export const environment = {
   apiUrl: 'http://127.0.0.1:8000/api',
   adminEmails: ['admin@nzolanet.com'],
   adminUsernames: ['admin', 'administrador']
- // apiUrl: 'http://127.0.0.1:8000/api'
    //apiUrl: 'https://nzolanet-production.up.railway.app/api'
 };
