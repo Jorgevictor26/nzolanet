1012f1aca8531ee4a50af38cc6ebb7c90f859813
Jorge Victor <jorgekvictor26@gmail.com>
2026-06-03 17:59:28 +0100

    Solving conflicts

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,10 +1,7 @@
 export const environment = {
-<<<<<<< HEAD
   apiUrl: 'http://127.0.0.1:8000/api',
   adminEmails: ['admin@nzolanet.com'],
   adminUsernames: ['admin', 'administrador']
-=======
  // apiUrl: 'http://127.0.0.1:8000/api'
-   apiUrl: 'https://nzolanet-production.up.railway.app/api'
->>>>>>> c64354ad29d0b455d05f7567b99faf3908ca6faf
+   //apiUrl: 'https://nzolanet-production.up.railway.app/api'
 };
